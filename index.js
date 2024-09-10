d3acd562f7042dfa516fa0a7994e4a54625fea29
nada <louatinada.70@gmail.com>
2024-09-10 13:35:39 +0100

    commit 1

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Configuration de CORS
+app.use(cors());
+
+// Middleware pour analyser les JSON
+app.use(express.json());
+
+// Démarrer le serveur et se connecter à MongoDB
+app.listen(PORT, async () => {
+    try {
+        await mongoose.connect(
+            process.env.MONGODB_URI,  // Assurez-vous que cette variable est correctement définie dans votre fichier .env
+            {
+                dbName: 'compteurs'
+            }
+        );
+        console.log(`MongoDB connecté et serveur démarré sur le port ${PORT}`);
+    } catch (error) {
+        console.error('Échec de connexion à MongoDB :', error.message);
+    }
+});
